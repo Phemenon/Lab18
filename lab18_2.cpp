bc7aeeb14c661fc02a2e8efeefe457857b6f2cfc
Phenomenon <xiii15499@gmail.com.com>
2025-02-08 20:26:01 +0700

    lab18_2

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,12 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *a, Rect *b){
+    double x_overlap = max(0.0, min(a->x + a->w, b->x + b->w) - max(a->x, b->x));
+    double y_overlap = max(0.0, min(a->y, b->y) - max(a->y - a->h, b->y - b->h)); 
 
-
+    return x_overlap * y_overlap; 
 
 }
+
+
